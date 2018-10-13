620538eda0e2a02fb1accabf68d46505e60aa1fe
bloo0178 <bloo0178@gmail.com>
2018-10-13 16:12:54 -0400

    101318 @ 1612

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/api.js');
-
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const app = express();
 
@@ -15,6 +15,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 require('dotenv').config();
 
 app.use('/public', express.static(process.cwd() + '/public'));
+
+/*Set content security policy to only allow loading of scripts
+and css from my server.*/
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: [" 'self' "], 
+        scriptSrc: [" 'self' "],
+        styleSrc: [" 'self' "]
+    }
+}))
 
 //Index page
 app.route('/')
